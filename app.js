fc0cf2b912e4b547ddc684960732ec94b62eee17
satyam123p <satyamkumar13569@gmail.com>
2025-05-27 14:50:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-if (!isNil(OpenApiToken)) {
-                logger.info('[OpenApiRequest]');
-                const openApiOrg = req.header('organizationId');
-                const openApiApp = req.header('appId');
-                if (!openApiOrg || !openApiApp) {
-                    logger.error('[ERROR]: Missing Organization Id or App Id in header');
-                    throw new Error('Unauthorized');
-                }
-                const redisOpenApiKey = 'OpenApi_' + openApiOrg + '_' + openApiApp;
-                const tokenFromRedis = await redisService.get(redisOpenApiKey);
-                if (!tokenFromRedis || tokenFromRedis !== OpenApiToken) {
-                    logger.error('[ERROR]: OpenAPI token mismatch or not found');
-                    throw new Error('Unauthorized');
-                }
-                next();
-                return;
-            } convert it into python
+const authResult = accessControlList[method.toUpperCase()] ? accessControlList[method.toUpperCase()].some((api) => {
+                            const urlMatch = match(api, { decode: decodeURIComponent });
+                            let originalUrlWithoutQuery = originalUrl.split('?')[0];
+                            return urlMatch(originalUrlWithoutQuery) != false;
+                            })
+                            : 
+                            false;
